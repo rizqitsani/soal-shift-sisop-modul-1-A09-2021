a2896e8acfb0f568ecc2add1e4df3f3c128f07b8
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-03-25 23:05:02 +0700

    Update major nomer 1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -8,16 +8,25 @@ regex3='(?<=\().+?(?=\))' # Regular expression untuk mendapatkan username didala
 # grep -oP "(ERROR |INFO)(.*)" syslog.log
 
 
-# 1. B & E
+# 1. B
+error_message() {
+	grep -oP "$regex2" syslog.log | sort | uniq -c | sort -nr | sed 's/^  *\([0-9]*\) *\(.*\)/\2,\1/' >> error_message.csv
+}
+
+# 1. C
+user_statistic() {
+	grep -oP "$regex3" syslog.log | sort | uniq | while read i 
+	do
+		echo "$i" | tr '\n' ','
+		grep "$i" syslog.log | grep "INFO" | wc -l | tr '\n' ','
+		grep "$i" syslog.log | grep "ERROR" | wc -l 
+	done >> user_statistic.csv
+}
+
+# 1. D
 echo "Error, Count" > error_message.csv
-grep -oP "$regex2" syslog.log | sort | uniq -c | sort -nr | sed 's/^  *\([0-9]*\) *\(.*\)/\2,\1/' >> error_message.csv
+error_message
 
-
-# 1. C & F
+# 1. F
 echo "Username,INFO,ERROR" > user_statistic.csv
-grep -oP "$regex3" syslog.log | sort | uniq | while read i 
-do
-	echo "$i" | tr '\n' ','
-	grep "$i" syslog.log | grep "INFO" | wc -l | tr '\n' ','
-	grep "$i" syslog.log | grep "ERROR" | wc -l 
-done >> user_statistic.csv
+user_statistic
